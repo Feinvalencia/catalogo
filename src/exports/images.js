e75f0fbcabf5bf180bda91247070f48efa3ddb1a
Feiner Valencia <feinvalencia@gmail.com>
2023-05-03 15:21:15 -0500

    adjust

--- src/exports/images.js
+++ src/exports/images.js
@@ -3,4 +3,8 @@ import image2 from '../assets/2.jpeg';
 import image3 from '../assets/3.jpeg';
 import image4 from '../assets/4.jpeg';
 
-export default [image1, image2, image3, image4];
+
+  declare module type Image = {
+    [image1, image2, image3, image4];
+  };
+
